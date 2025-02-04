8206e79c2cafae9f22e42791277d41ac35160937
AmelieSavaryTolstoy <tolstoy.a.savary@gmail.com>
2025-02-04 16:07:20 +0100

    le reste des fichier

--- api/articles/update.php
+++ api/articles/update.php
@@ -1,5 +1,4 @@
 <?php
-
     require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
     require_once '../../functions/ctrlSaisies.php';
 
@@ -20,5 +19,5 @@
 
 
     sql_update('ARTICLE','libTitrArt= "$libTitrArt", dtCreaArt= "$dtCreaArt", dtMajArt= "$dtMajArt", libChapoArt= "$libChapoArt", libAccrochArt= "$libAccrochArt", parag1Art= "$parag1Art", libSsTitr1Art= "$libSsTitr1Art", parag2Art= "$parag2Art", libSsTitr2Art= "$libSsTitr2Art", parag3Art= "$parag3Art", libConclArt= "$libConclArt", urlPhotArt= "$urlPhotArt", libThem= "$libThem"', "numArt = $numArt");
-
+    
     header('Location: ../../views/backend/articles/list.php');
